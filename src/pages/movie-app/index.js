0adfe8c28b985214881969eba93f20916430bddc
jalexguerrero <johnalexguerrero@gmail.com>
2023-05-22 22:47:28 -0500

    search movies

--- src/pages/movie-app/index.js
+++ src/pages/movie-app/index.js
@@ -11,39 +11,60 @@ export default function MovieApp({ data }) {
     setMovies(data.results)
   },[])
 
-  const handleQuery = (event)=>{
+  const handleQuery = async(event)=>{
     setQuery(event.target.value)
   }
 
   const handleSearch = async ()=>{
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=${query}`)
-    const data = await res.json()
+    const data_search = await res.json()
+    setMovies(data_search.results)
 
-    setMovies(data.results)
     console.log(movies)
   }
 
+  const hasMovies = movies.length > 0
+
+  console.log(hasMovies)
+
+
   return (
     <>
-      <form onSubmit={handleSearch}>
-        <label for="first">Search:</label>
-        <input type="text" value={query} onInput={handleQuery}/>
-        <button type='submit'>Submit</button>
-      </form>
+      <div>
+        <input
+          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          value={query}
+          onChange={handleQuery}
+          placeholder='Search Movie'
+        />
+        <button type='submit' onClick={handleSearch}>Submit</button>
+      </div>
 
       <h1>Lista de artículos</h1>
 
-      {movies.map(({id, original_title, title, poster_path}) =>(
-        <div key={id}>
-          <Image
-            src={'https://image.tmdb.org/t/p/w1280'+poster_path}
-            height={200}
-            width={200}
-            alt={original_title}
-          ></Image>
-          <p>{title}</p>
-        </div>
-      ))}
+      <div className="px-10 pt-5 flex flex-col gap-10">
+        {
+          hasMovies
+          ? movies.map(({id, original_title, title, poster_path}) =>(
+              <div key={id}>
+                <Image
+                  src={'https://image.tmdb.org/t/p/w1280/'+poster_path}
+                  height={200}
+                  width={200}
+                  alt={original_title}
+                ></Image>
+                <p>{title}</p>
+              </div>
+            ))
+          : (
+              <div className=" bg-red-400 text-white text-center p-10">
+                <p>No se encontraron pokemones que coincidan con la búsqueda</p>
+                <span className=" text-md font-thin">Prueba buscar por otro nombre</span>
+              </div>
+          )
+        }
+      </div>
     </>
   )
 }
